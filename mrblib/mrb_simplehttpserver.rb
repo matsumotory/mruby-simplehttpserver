c8b45f0eb5b161284126f88ee3a275424806cdf4
Sebastián Katzer <katzer@users.noreply.github.com>
2017-09-21 20:47:59 +0200

    Do not create a new instance of HTTP::Parser for each new request

--- mrblib/mrb_simplehttpserver.rb
+++ mrblib/mrb_simplehttpserver.rb
@@ -19,6 +19,7 @@ class SimpleHttpServer
     @port     = config[:port]
     @nonblock = config[:nonblock]
     @app      = config[:app]
+    @parser   = HTTP::Parser.new
     @server   = nil
   end
 
@@ -62,7 +63,7 @@ class SimpleHttpServer
   end
 
   def on_data(data)
-    request               = HTTP::Parser.new.parse_request(data)
+    request               = @parser.parse_request(data)
     env                   = request_to_env(request)
     status, headers, body = @app.call(env)
 
