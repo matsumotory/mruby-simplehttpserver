dd31d39c516d3530acc01ec14df34b1489b243c3
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-03-29 03:52:25 +0900

    Test for SimpleHttpServer#initialize

--- test/mrb_simplehttpserver.rb
+++ test/mrb_simplehttpserver.rb
@@ -1,5 +1,36 @@
-##  
+##
 ## SimpleHttpServer Test
 ##
 
-# TODO: Add tests
+## Setup configuration
+host = 'localhost'
+port = 8000
+app = Proc.new do |env|
+  code = 200
+  headers = { 'Server' => 'mruby-simplehttpserver' }
+  path = env['PATH_INFO']
+  method = env['REQUEST_METHOD']
+  body = nil
+
+  case path
+  when '/mruby'
+    body = "Hello mruby World.\n"
+  when '/html'
+    headers['Content-type'] = 'text/html; charset=utf-8'
+    body = "<H1>Hello mruby World.</H1>\n"
+  when '/notfound'
+    # Custom error response message
+    body = "Not Found on this server: #{path}\n"
+    code = 404
+  end
+
+  [code, headers, [body]]
+end
+
+assert 'SimpleHttpServer#initialize' do
+  server = SimpleHttpServer.new(server_ip: host, port: port, app: app)
+
+  assert_kind_of Hash, server.config
+  assert_equal host, server.host
+  assert_equal port, server.port
+end
