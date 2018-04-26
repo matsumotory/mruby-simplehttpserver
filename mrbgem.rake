7e6b07e407cfa7f905b32ab9e7a4bb5000d6d6d6
Sebastián Katzer <katzer.sebastian@googlemail.com>
2018-04-26 10:36:00 +0200

    Remove circular dependency with mruby-shelf

--- mrbgem.rake
+++ mrbgem.rake
@@ -3,12 +3,10 @@ require "#{MRUBY_ROOT}/lib/mruby/source"
 MRuby::Gem::Specification.new('mruby-simplehttpserver') do |spec|
   spec.license = 'MIT'
   spec.authors = ['MATSUMOTO Ryosuke', 'KATZER Sebastian']
-  spec.version = '1.0.0'
 
   spec.add_dependency('mruby-string-ext', core: 'mruby-string-ext')
   spec.add_dependency('mruby-time', core: 'mruby-time')
   spec.add_dependency('mruby-http')
-  spec.add_dependency('mruby-shelf')
 
   if MRuby::Source::MRUBY_VERSION >= '1.4.0'
     spec.add_dependency('mruby-io', core: 'mruby-io')
--- mrblib/mrb_simplehttpserver.rb
+++ mrblib/mrb_simplehttpserver.rb
@@ -8,10 +8,74 @@ class SimpleHttpServer
   SEP          = "\r\n".freeze
   RECV_BUF     = 1024
   HTTP_VERSION = 'HTTP/1.1'.freeze
+  HTTP_DATE    = 'Date'.freeze
   ROOT_PATH    = '/'.freeze
 
+  # Every standard HTTP code mapped to the appropriate message.
+  HTTP_STATUS_CODES = {
+    100 => 'Continue'.freeze,
+    101 => 'Switching Protocols'.freeze,
+    102 => 'Processing'.freeze,
+    200 => 'OK'.freeze,
+    201 => 'Created'.freeze,
+    202 => 'Accepted'.freeze,
+    203 => 'Non-Authoritative Information'.freeze,
+    204 => 'No Content'.freeze,
+    205 => 'Reset Content'.freeze,
+    206 => 'Partial Content'.freeze,
+    207 => 'Multi-Status'.freeze,
+    208 => 'Already Reported'.freeze,
+    226 => 'IM Used'.freeze,
+    300 => 'Multiple Choices'.freeze,
+    301 => 'Moved Permanently'.freeze,
+    302 => 'Found'.freeze,
+    303 => 'See Other'.freeze,
+    304 => 'Not Modified'.freeze,
+    305 => 'Use Proxy'.freeze,
+    307 => 'Temporary Redirect'.freeze,
+    308 => 'Permanent Redirect'.freeze,
+    400 => 'Bad Request'.freeze,
+    401 => 'Unauthorized'.freeze,
+    402 => 'Payment Required'.freeze,
+    403 => 'Forbidden'.freeze,
+    404 => 'Not Found'.freeze,
+    405 => 'Method Not Allowed'.freeze,
+    406 => 'Not Acceptable'.freeze,
+    407 => 'Proxy Authentication Required'.freeze,
+    408 => 'Request Timeout'.freeze,
+    409 => 'Conflict'.freeze,
+    410 => 'Gone'.freeze,
+    411 => 'Length Required'.freeze,
+    412 => 'Precondition Failed'.freeze,
+    413 => 'Payload Too Large'.freeze,
+    414 => 'URI Too Long'.freeze,
+    415 => 'Unsupported Media Type'.freeze,
+    416 => 'Range Not Satisfiable'.freeze,
+    417 => 'Expectation Failed'.freeze,
+    421 => 'Misdirected Request'.freeze,
+    422 => 'Unprocessable Entity'.freeze,
+    423 => 'Locked'.freeze,
+    424 => 'Failed Dependency'.freeze,
+    426 => 'Upgrade Required'.freeze,
+    428 => 'Precondition Required'.freeze,
+    429 => 'Too Many Requests'.freeze,
+    431 => 'Request Header Fields Too Large'.freeze,
+    451 => 'Unavailable for Legal Reasons'.freeze,
+    500 => 'Internal Server Error'.freeze,
+    501 => 'Not Implemented'.freeze,
+    502 => 'Bad Gateway'.freeze,
+    503 => 'Service Unavailable'.freeze,
+    504 => 'Gateway Timeout'.freeze,
+    505 => 'HTTP Version Not Supported'.freeze,
+    506 => 'Variant Also Negotiates'.freeze,
+    507 => 'Insufficient Storage'.freeze,
+    508 => 'Loop Detected'.freeze,
+    510 => 'Not Extended'.freeze,
+    511 => 'Network Authentication Required'.freeze
+  }.freeze
+
   def self.status_line(code = 200)
-    "#{HTTP_VERSION} #{code} #{Shelf::Utils::HTTP_STATUS_CODES[code]}"
+    "#{HTTP_VERSION} #{code} #{HTTP_STATUS_CODES[code]}"
   end
 
   def initialize(config)
@@ -76,7 +140,7 @@ class SimpleHttpServer
   end
 
   def create_response(code, headers, body)
-    headers[Shelf::DATE] ||= http_date
+    headers[HTTP_DATE] ||= http_date
 
     header_ary = []
     headers.each { |k, v| header_ary << ["#{k}:#{v}"] if v }
